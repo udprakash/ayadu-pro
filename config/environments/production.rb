f7f055ed98cb4779b2225d560131002e665b0da6
udaya prakash <ud@thetraces.com>
2014-01-01 23:49:16 +0530

    changed about home and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Ayadu::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { :host => 'http://udaya.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'http://udaya1.herokuapp.com/' }
 
   config.paperclip_defaults = {
   :storage => :s3,
